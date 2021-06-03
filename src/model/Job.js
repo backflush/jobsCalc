d80a1b49b31e1a533a79fa407b2474354338b393
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-03 15:24:02 -0300

    Finished the Job model

---
+++ src/model/Job.js
@@ -0,0 +1,58 @@
+const { update, create } = require("../controllers/JobController");
+const Database = require("../db/config");
+
+module.exports = {
+    async get(){
+        const db = await Database();
+
+        const data = await db.all("SELECT * FROM jobs");
+
+        await db.close();
+        return data.map(job => ({
+            id: job.id,
+            name: job.name,
+            "daily-hours": job.daily_hours,
+            "total-hours": job.total_hours,
+            createdAt: job.createdAt
+        }));
+    },
+
+    async update(updatedJob, jobId){
+        const db = await Database();
+
+        await db.run(`UPDATE jobs SET 
+            name = "${updatedJob.name}", 
+            daily_hours = ${updatedJob["daily-hours"]},
+            total_hours = ${updatedJob["total-hours"]}
+            WHERE id = ${jobId}
+        `);
+
+        await db.close();
+    },
+
+    async delete(id){
+        const db = await Database();
+
+        await db.run(`DELETE FROM jobs WHERE id = ${id}`)
+
+        await db.close();
+    },
+    
+    async create(newJob){
+        const db = await Database();
+
+        await db.run(`INSERT INTO jobs (
+            name, 
+            daily_hours,
+            total_hours,
+            createdAt
+        ) VALUES (
+            "${newJob.name}",
+            ${newJob["daily-hours"]},
+            ${newJob["total-hours"]},
+            ${newJob.createdAt}
+        )`);
+
+        await db.close();
+    }
+}
