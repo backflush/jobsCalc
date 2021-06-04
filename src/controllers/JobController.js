77987e9d25e6c05a5ff53cbee77b055a78c79133
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-04 07:53:56 -0300

    Finished the controllers logic

---
+++ src/controllers/JobController.js
@@ -0,0 +1,52 @@
+const Job = require('../model/Job');
+const JobUtils = require('../utils/JobUtils');
+const Profile = require('../model/Profile');
+
+module.exports = {
+    create(req, res){
+        return res.render("job"); 
+    },
+    async save(req, res){
+        await Job.create({
+            name: req.body.name,
+            "daily-hours": req.body["daily-hours"],
+            "total-hours": req.body["total-hours"],
+            createdAt: Date.now()
+        });
+
+        return res.redirect("/");
+    },
+    async show(req, res){
+        const jobs = await Job.get();
+        const profile = await Profile.get();
+        const jobId = req.params.id;
+
+        const job = jobs.find(job => job.id === Number(jobId));
+        if(!job){
+            return res.send("Job not found!");
+        }
+
+        job.budget = JobUtils.calculateBudget(job, profile["value-hour"]);
+
+        return res.render("job-edit", { job }); 
+    },
+    async update(req, res){
+        const jobId = req.params.id;
+       
+        const updatedJob = {
+            name: req.body.name,
+            "total-hours": req.body["total-hours"],
+            "daily-hours": req.body["daily-hours"],
+        }
+        
+        await Job.update(updatedJob, jobId);
+        res.redirect("/job/" + jobId);
+    },
+    async delete(req, res){
+        const jobId = req.params.id;
+     
+        await Job.delete(jobId);
+
+        return res.redirect("/");
+    }
+}
---
+++ src/controllers/ProfileController.js
@@ -0,0 +1,24 @@
+const Profile = require("../model/Profile");
+
+module.exports = {
+    async index(req, res){
+        return res.render("profile", { profile: await Profile.get() });
+    },
+    async update(req, res){
+        const data = req.body;
+        const weeksPerYear = 52;
+        const weeksPerMonth= (weeksPerYear - data["vacation-per-year"])/12;
+        const weekTotalHours = data["hours-per-day"] * data["days-per-week"];
+        const monthlyTotalHours = weekTotalHours * weeksPerMonth;
+        const hourValue = data["monthly-budget"]/monthlyTotalHours;
+
+        const profile = await Profile.get();
+
+        await Profile.update({
+            ...profile,
+            ...req.body,
+            "value-hour": hourValue
+        });
+        return res.redirect("/profile");
+    }
+}
